12a06eeada1dfda31283bb2a6e407b0bb2d786d4
AlyneAlvarenga <alynevs@gmail.com>
2020-05-04 11:41:04 -0400

    first blog page created and reading from mongo

--- app.js
+++ app.js
@@ -4,11 +4,44 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//app config
 mongoose.connect('mongodb://localhost/blog_app', { useNewUrlParser: true, useUnifiedTopology: true });
-
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//mongoose/model config
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+// Blog.create({
+//   title: 'Test',
+//   image: 'https://placekitten.com/300/300',
+//   body: 'Hello this is a blog post'
+// });
+
+//RESTful routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+})
+
+app.get('/blogs', (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+
+    } else {
+      res.render('index', {blogs});
+
+    }
+  })
+})
 
 app.listen(3000, () => {
   console.log('listening!');
